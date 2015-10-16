45f165d752a34ab98812c2c9e0997ab5a1e51f29
stijnsanders <devel@yoy.be>
2015-10-16 20:57:27 +0200

    SQLiteBatch: skip whitespace (avoid 'not an error' on trailing semicolon)

--- SQLiteBatch/SQLiteBatchMain.pas
+++ SQLiteBatch/SQLiteBatchMain.pas
@@ -63,7 +63,7 @@ begin
         st:=TSQLiteStatement.Create(db,s,i);
         try
           l:=Length(s);
-          while (i<=l) and (s[i+1]<=' ') do inc(i);
+          while (i<l) and (s[i+1]<=' ') do inc(i);
           {
           t:='';
           for i:=0 to st.FieldCount-1 do
